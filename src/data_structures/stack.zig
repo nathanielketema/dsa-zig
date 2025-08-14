5dc2d6fb462f3ce61c5e10a238a0f099fc07edaf
Nathaniel Ketema <nathanielketema@gmail.com>
2025-08-14 11:27:54 -0500

    feat: implement push

--- src/data_structures/stack.zig
+++ src/data_structures/stack.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 const assert = std.debug.assert;
+const Allocator = std.mem.Allocator;
 
 pub fn Stack(comptime T: type) type {
     // You can pick which implementation to expose for the caller
@@ -7,19 +8,49 @@ pub fn Stack(comptime T: type) type {
     //return StackList(T);
 }
 
-fn StackLinkedList(comptime T: type) type {
+pub fn StackLinkedList(comptime T: type) type {
     const Node = struct {
         const Self = @This();
-        items: T,
+        value: T,
         next: ?*Self,
     };
 
     return struct {
-        head: ?Node,
+        head: ?*Node,
         /// Number of T items that can be stored
         capacity: u32,
         /// Number of T items that are currently stored
         count: u32,
+        allocator: Allocator,
+
+        const Self = @This();
+
+        pub fn init(allocator: Allocator, capacity: u32) Self {
+            return .{
+                .head = null,
+                .capacity = capacity,
+                .count = 0,
+                .allocator = allocator,
+            };
+        }
+
+        pub fn push(self: *Self, value: T) !void {
+            // This is a smart way to ensure if:
+            // - count == 0, then self.head must be null, and if
+            // - count != 0, then self.head must not be null
+            assert((self.count == 0) == (self.head == null));
+            assert(self.count < self.capacity);
+
+            const new_node = try self.allocator.create(Node);
+            new_node.* = Node{
+                .value = value,
+                .next = null
+            };
+
+            new_node.next = self.head;
+            self.head = new_node;
+            self.count += 1;
+        }
     };
 
     // Todo:
