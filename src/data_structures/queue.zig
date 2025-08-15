95149149011961fcc4335af3709c526c2f2c526c
Nathaniel Ketema <nathanielketema@gmail.com>
2025-08-14 19:49:28 -0500

    feat: all tests pass for queue

--- src/data_structures/queue.zig
+++ src/data_structures/queue.zig
@@ -12,13 +12,6 @@ pub fn Queue(comptime T: type) type {
         next: ?*Self,
     };
 
-    // Todo:
-    // - peek
-    // - peek_last
-    // - empty
-    // - contains
-    // - print
-
     return struct {
         in: ?*Node,
         out: ?*Node,
@@ -56,15 +49,15 @@ pub fn Queue(comptime T: type) type {
                 return QueueError.QueueFull;
             }
 
-            const new_node = self.allocator.create(Node);
+            const new_node = try self.allocator.create(Node);
             new_node.* = Node{
                 .value = value,
                 .next = null,
             };
 
-            if (self.in) |in| {
-                in.next = new_node;
-                in = new_node;
+            if (self.in) |*in| {
+                in.*.next = new_node;
+                in.* = new_node;
             } else {
                 self.in = new_node;
                 self.out = new_node;
@@ -85,5 +78,115 @@ pub fn Queue(comptime T: type) type {
             self.count -= 1;
             return value;
         }
+
+        pub fn peek(self: Self) ?T {
+            assert((self.count == 0) == (self.in == null) and
+                (self.in == null) == (self.out == null));
+
+            if (self.out) |out| {
+                return out.value;
+            } else return null;
+        }
+
+        pub fn peek_last(self: Self) ?T {
+            assert((self.count == 0) == (self.in == null) and
+                (self.in == null) == (self.out == null));
+
+            if (self.in) |in| {
+                return in.value;
+            } else return null;
+        }
+
+        pub fn empty(self: Self) bool {
+            assert((self.count == 0) == (self.in == null) and
+                (self.in == null) == (self.out == null));
+            return self.count == 0;
+        }
+
+        pub fn contains(self: Self, needle: T) bool {
+            assert((self.count == 0) == (self.in == null) and
+                (self.in == null) == (self.out == null));
+
+            var current_node = self.out;
+            while (current_node) |node| {
+                current_node = node.next;
+                if (node.value == needle) return true;
+            } else return false;
+        }
+
+        pub fn print(self: Self) void {
+            assert((self.count == 0) == (self.in == null) and
+                (self.in == null) == (self.out == null));
+
+            std.debug.print(
+                \\out
+                \\ | 
+                \\ v
+                \\
+            , .{});
+
+            var current_node = self.out;
+            while (current_node) |node| {
+                current_node = node.next;
+                std.debug.print(" {d} -> ", .{node.value});
+            }
+
+            std.debug.print("null <- in\n\n", .{});
+        }
     };
+}
+
+test "test queue" {
+    var gpa: std.heap.DebugAllocator(.{}) = .init;
+    defer assert(gpa.deinit() == .ok);
+    const allocator = gpa.allocator();
+
+    var queue = Queue(u8).init(allocator, 10);
+    defer queue.deinit();
+
+    try testing.expect(queue.capacity == 10);
+    try testing.expect(queue.count == 0);
+    try testing.expect(queue.empty());
+    try testing.expect(queue.pop() == null);
+    try testing.expect(queue.peek() == null);
+    try testing.expect(queue.peek_last() == null);
+
+    try queue.push(1);
+    try queue.push(2);
+    try queue.push(3);
+    try queue.push(4);
+    try queue.push(5);
+    try queue.push(6);
+
+    queue.print();
+
+    try testing.expect(queue.count == 6);
+    try testing.expect(queue.contains(6));
+    try testing.expectEqual(queue.peek(), 1);
+    try testing.expectEqual(queue.peek_last().?, 6);
+
+    try testing.expectEqual(queue.pop().?, 1);
+    try testing.expectEqual(queue.pop().?, 2);
+
+    try testing.expectEqual(queue.peek().?, 3);
+    try testing.expectEqual(queue.peek_last().?, 6);
+    try testing.expect(queue.count == 4);
+
+    try testing.expect(!queue.contains(1));
+    try testing.expect(queue.contains(4));
+    try testing.expect(queue.contains(6));
+    try testing.expect(!queue.empty());
+
+    queue.print();
+
+    try queue.push(5);
+    try queue.push(6);
+    try queue.push(7);
+    try queue.push(8);
+    try queue.push(9);
+    try queue.push(10);
+
+    try testing.expect(queue.count == queue.capacity);
+
+    queue.print();
 }
