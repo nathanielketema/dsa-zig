a3a91862033f758a0b6f462edbc32b9272133d1f
Nathaniel Ketema <nathanielketema@gmail.com>
2025-09-12 14:12:26 -0500

    feat: add bubble sort

--- src/algorithms/bubble_sort.zig
+++ src/algorithms/bubble_sort.zig
@@ -3,7 +3,7 @@ const assert = std.debug.assert;
 const testing = std.testing;
 
 pub fn bubble_sort(comptime T: type, items: []T) void {
-    assert(items.len > 0);
+    if (items.len <= 0) return;
     var swapped: bool = undefined;
     for (0..items.len - 1) |i| {
         swapped = false;
@@ -25,4 +25,27 @@ test "test" {
     bubble_sort(u8, &array);
 
     try testing.expectEqualSlices(u8, &[_]u8{ 1, 2, 3, 4, 5 }, &array);
+
+    var list = [_]u8{ 'n', 'a', 't', 'h', 'a', 'n', 'i', 'e', 'l' };
+    bubble_sort(u8, &list);
+
+    try testing.expectEqualSlices(
+        u8,
+        &[_]u8{ 'a', 'a', 'e', 'h', 'i', 'l', 'n', 'n', 't' },
+        &list,
+    );
+}
+
+test "fuzzing" {
+    var random = std.Random.DefaultPrng.init(23);
+    var input: [1_000]u32 = undefined;
+    _ = &input;
+    for (0..input.len) |i| {
+        input[i] = random.random().uintAtMost(u32, 1_000);
+    }
+
+    bubble_sort(u32, &input);
+    for (0..input.len - 1) |i| {
+        try testing.expect(input[i] <= input[i + 1]);
+    }
 }
