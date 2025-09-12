7574f60331293b534b591a844becdf4c3cf35659
Nathaniel Ketema <nathanielketema@gmail.com>
2025-09-12 14:11:36 -0500

    refactor: rename the test test step

--- build.zig
+++ build.zig
@@ -43,21 +43,29 @@ pub fn build(b: *std.Build) void {
         })
     });
 
-    const exe_bst_link = b.addTest(.{
+    const exe_bst = b.addTest(.{
         .root_module = b.createModule(.{
-            .root_source_file = b.path("src/data-structures/binary_tree_link.zig"),
+            .root_source_file = b.path("src/data-structures/binary_tree.zig"),
             .target = target,
             .optimize = optimize,
         })
     });
 
+    const exe_bubble = b.addTest(.{
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/algorithms/bubble_sort.zig"),
+            .target = target,
+            .optimize = optimize,
+        })
+    });
 
     const build_steps = .{
         .run_step = b.step("run", "Run main"),
         .test_step = b.step("test", "Test main"),
         .test_stack_step = b.step("test_stack", "Test Stack"),
         .test_queue_step = b.step("test_queue", "Test Queue"),
-        .test_bst_link_step = b.step("test_bst_link", "Test Binary Search Tree Linked List"),
+        .test_bst_step = b.step("test_bst", "Test Binary Search Tree"),
+        .test_bubble_step = b.step("test_bubble", "Test Bubble Sort"), 
     };
 
     const cmds = .{
@@ -65,12 +73,14 @@ pub fn build(b: *std.Build) void {
         .test_cmd = b.addRunArtifact(exe),
         .test_stack_cmd = b.addRunArtifact(exe_stack),
         .test_queue_cmd = b.addRunArtifact(exe_queue),
-        .test_bst_link_cmd = b.addRunArtifact(exe_bst_link),
+        .test_bst_cmd = b.addRunArtifact(exe_bst),
+        .test_bubble_cmd = b.addRunArtifact(exe_bubble),
     };
 
     build_steps.run_step.dependOn(&cmds.run_cmd.step);
     build_steps.test_step.dependOn(&cmds.test_cmd.step);
     build_steps.test_stack_step.dependOn(&cmds.test_stack_cmd.step);
     build_steps.test_queue_step.dependOn(&cmds.test_queue_cmd.step);
-    build_steps.test_bst_link_step.dependOn(&cmds.test_bst_link_cmd.step);
+    build_steps.test_bst_step.dependOn(&cmds.test_bst_cmd.step);
+    build_steps.test_bubble_step.dependOn(&cmds.test_bubble_cmd.step);
 }
