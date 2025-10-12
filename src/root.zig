7cebff171a631cfb3e1fc4d3c41fdf9382af3297
Nathaniel Ketema <nathanielketema@gmail.com>
2025-10-12 11:06:50 -0500

    feat: expose binary search tree to module

--- src/root.zig
+++ src/root.zig
@@ -5,6 +5,10 @@ const testing = std.testing;
 // Available Data Structures
 pub const Stack = @import("data-structures/stack.zig").Stack;
 pub const Queue = @import("data-structures/queue.zig").Queue;
+pub const BinarySearchTree = @import("data-structures/binary_search_tree.zig").BinarySearchTree;
+pub const BinarySearchTreeWithImplementation = @import(
+    "data-structures/binary_search_tree.zig",
+).BinarySearchTreeWithImplementation;
 
 // Available Algorithms
 pub const bubble_sort = @import("algorithms/bubble_sort.zig").bubble_sort;
